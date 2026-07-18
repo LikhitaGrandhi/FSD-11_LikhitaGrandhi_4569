6e552131fff83d141f70da4f66e4ad5474ca8576
Likhita <likhita1907@gmail.com>
2026-07-18 09:07:16 +0530

    git

--- annotations.ts
+++ annotations.ts
@@ -1,3 +1,4 @@
+// Program 1: Variable Annotations
 let na: string = "Likhita";
 let age_student: number = 20;
 let isStudent: boolean = true;
@@ -6,14 +7,17 @@ console.log("Name:", na);
 console.log("Age:", age_student);
 console.log("Student:", isStudent);
 
+// Program 2: Function Return Type Annotation
 function add(a: number, b: number): number {
     return a + b;
 }
+
 let result = add(10, 20);
 console.log("Sum:", result);
 
-
+// Program 3: Array Type Annotation
 let numbers: number[] = [10, 20, 30, 40];
+
 for (let num of numbers) {
     console.log(num);
 }
--- datatypes_simple.ts
+++ datatypes_simple.ts
@@ -1,33 +1,42 @@
+// Program 1: Addition of two numbers
 let a: number = 10;
 let b: number = 20;
 let sum: number = a + b;
 console.log("Sum :", sum);
 
+// Program 2: Area of a Circle
 let r: number = 4;
-let pi : number = 1.4;
-let area : number = pi * r * r;
-console.log("Area : ", area);
+let pi: number = 1.4;
+let area: number = pi * r * r;
+console.log("Area :", area);
 
-let m : number = 7.9;
+// Program 3: Decimal Number
+let m: number = 7.9;
 console.log(m);
 
+// Program 4: Display a String
 let nam: string = "Likhita";
 console.log("Name:", nam);
 
+// Program 5: Concatenate Two Strings
 let firstName: string = "Likhita";
 let lastName: string = "Grandhi";
 let fullName: string = firstName + " " + lastName;
 console.log("Full Name:", fullName);
 
+// Program 6: Find Length of a String
 let cou: string = "TypeScript";
 console.log("Length =", cou.length);
 
-let isFriend : boolean = true;
+// Program 7: Boolean Value (True)
+let isFriend: boolean = true;
 console.log(isFriend);
 
+// Program 8: Check Voting Eligibility
 let age: number = 19;
 let eligible: boolean = age >= 18;
 console.log("Eligible to Vote:", eligible);
 
-let notFriend : boolean = false;
+// Program 9: Boolean Value (False)
+let notFriend: boolean = false;
 console.log(notFriend);
