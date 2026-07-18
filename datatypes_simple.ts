3b19a26a9ba8b68c6f34be0d9cc908312f13a4ad
Likhita <likhita1907@gmail.com>
2026-07-18 08:45:21 +0530

    git

---
+++ datatypes_simple.ts
@@ -0,0 +1,33 @@
+let a: number = 10;
+let b: number = 20;
+let sum: number = a + b;
+console.log("Sum :", sum);
+
+let r: number = 4;
+let pi : number = 1.4;
+let area : number = pi * r * r;
+console.log("Area : ", area);
+
+let m : number = 7.9;
+console.log(m);
+
+let nam: string = "Likhita";
+console.log("Name:", nam);
+
+let firstName: string = "Likhita";
+let lastName: string = "Grandhi";
+let fullName: string = firstName + " " + lastName;
+console.log("Full Name:", fullName);
+
+let cou: string = "TypeScript";
+console.log("Length =", cou.length);
+
+let isFriend : boolean = true;
+console.log(isFriend);
+
+let age: number = 19;
+let eligible: boolean = age >= 18;
+console.log("Eligible to Vote:", eligible);
+
+let notFriend : boolean = false;
+console.log(notFriend);
